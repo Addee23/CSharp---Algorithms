4d851bd4ac794d1de65e03375cba28adf2b5c4db
Adina Moldovan <adiiinaaaa86@gmail.com>
2024-12-16 12:53:28 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Uppgift_Algoritmer
+{
+    public class Program
+    {
+        List<Student> students = new List<Student>();
+
+        static void Main(string[] args)
+        {
+
+            //Lägger till studenter
+            AddStudent("Adina", 95);
+            AddStudent("Anna", 68);
+            AddStudents("Lars", 87);
+            AddStudents("John", 78);
+
+
+            Console.WriteLine("\nStudenter innan sortering: ");
+            PrintStudents();
+
+            //Sorterar studenter
+            BubbleSortStudents();
+
+            //Efter sortering
+
+            Console.WriteLine("\nStudenter efter sortering: ");
+            PrintStudents();
+        }
+        //Metoder
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_Algoritmer
+{
+    public class Student
+    {
+        //Egenskaper för namn och betyg
+        public string Name { get; set; }
+        public int Grade { get; set; }
+
+        //Konstruktor
+        public Student(string name, int grade)
+        {
+            Name = name;
+            Grade = grade;
+        }
+
+
+
+
+
+    }
+}
